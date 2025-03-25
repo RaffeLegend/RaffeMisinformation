c22f991cc81eebbbfbce75a0f1038bcda16c3cf2
Yiwei <oldgirafferiver@gmail.com>
2025-03-25 17:43:07 +0000

    update the data

--- src/dataset/data_tools.py
+++ src/dataset/data_tools.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
 # Load the dataset
-dataset = load_dataset('your_dataset_name')
+dataset = load_dataset("liuxuannan/MMFakeBench", "MMFakeBench_test")
 
 # Print the first example in the dataset
 print(dataset['train'][0])
